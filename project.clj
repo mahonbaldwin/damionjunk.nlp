6c991171f21e1d51c21d428a2dada2b0189277d1
Tobias Quinn <tobias@tobiasquinn.com>
2018-04-16 13:01:24 +0100

    upgraded clojure and stanford-corenlp to latest versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject damionjunk/nlp "0.3.0"
+(defproject damionjunk/nlp "0.3.1-SNAPSHOT"
   :description "Code examples using Stanford CoreNLP"
   :url "https://github.com/damionjunk/damionjunk.nlp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ["-Xmx1024m" "-server"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.5.2"]
-                 [edu.stanford.nlp/stanford-corenlp "3.5.2" :classifier "models"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [edu.stanford.nlp/stanford-corenlp "3.9.1"]
+                 [edu.stanford.nlp/stanford-corenlp "3.9.1" :classifier "models"]
                  [edu.cmu.cs/ark-tweet-nlp "0.3.2"]]
   :profiles {:dev {:dependencies [[expectations "2.1.1"]]}}
   :plugins [[lein-expectations "0.0.8"]])
