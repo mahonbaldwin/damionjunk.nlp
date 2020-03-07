68dadd03e696e2cf37ad473d6cc707f84131c330
mahon <mbbaldwin@gmail.com>
2020-03-07 15:58:49 -0800

    Updated deps.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ["-Xmx1024m" "-server"]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.9.1"]
-                 [edu.stanford.nlp/stanford-corenlp "3.9.1" :classifier "models"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [edu.stanford.nlp/stanford-corenlp "3.9.2"]
+                 [edu.stanford.nlp/stanford-corenlp "3.9.2" :classifier "models"]
                  [edu.cmu.cs/ark-tweet-nlp "0.3.2"]]
   :profiles {:dev {:dependencies [[expectations "2.1.1"]]}}
   :plugins [[lein-expectations "0.0.8"]])
